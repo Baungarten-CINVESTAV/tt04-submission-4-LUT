10fb72cd2c6e492c5b3e8ffd2fd00f729896cc61
Baungarten-CINVESTAV <101527680+Baungarten-CINVESTAV@users.noreply.github.com>
2023-08-28 16:57:32 -0600

    Update tt_um_4_LUT_Baungarten.v

--- src/tt_um_4_LUT_Baungarten.v
+++ src/tt_um_4_LUT_Baungarten.v
@@ -1,11 +1,28 @@
-module tt_um_4_LUT_Baungarten(
-input [3:0] i_addr_load_data,
-input i_Data,
-input [3:0] i_LUT,
-input i_config_enable,
-output reg o_Data
-);
 
+
+module tt_um_4_LUT_Baungarten(
+    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
+    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
+    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
+    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+	
+wire [3:0] i_addr_load_data;
+wire i_Data;
+wire [3:0] i_LUT;
+wire i_config_enable;
+reg o_Data;
+	
+assign i_addr_load_data = ui_in[3:0];
+assign i_Data = ui_in[4];
+assign i_config_enable = ui_in[5];
+assign uio_oe[3:0] = 3'b000;
+assign i_LUT = uio_in[3:0]; 	
+	assign uo_out[0] = o_Data;
 reg [15:0] r_data;
 always@*
 begin
